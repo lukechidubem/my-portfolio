56a84599508227ba3594dc84834418df437463a1
lukechidubem <lukechidubem@gmail.com>
2023-01-03 06:57:43 +0100

    modified home profile picture

--- style-switcher.js
+++ style-switcher.js
@@ -1,6 +1,7 @@
 // =================== toggle style switcher=================
 
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+
 styleSwitcherToggle.addEventListener("click", () => {
   document.querySelector(".style-switcher").classList.toggle("open");
 });
